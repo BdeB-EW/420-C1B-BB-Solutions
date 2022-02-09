bf0b6d767894f41ab2f0c0fad3d2a336c3c9c603
Eric Wenaas <eric.wenaas@bdeb.qc.ca>
2022-02-09 13:59:45 -0500

    Exercice 2 complété

--- Module3/Exercice2/Assets/Scripts/RotationCube.cs
+++ Module3/Exercice2/Assets/Scripts/RotationCube.cs
@@ -2,21 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Classe servant à faire une démonstration sur les rotations
+/// 
+/// Auteurs: Enseignants de 420-C1B-BB
+/// </summary>
 public class RotationCube : MonoBehaviour
 {
+    /// <summary>
+    /// L'axe de rotation de l'objet
+    /// </summary>
     [SerializeField] private Vector3 axeRotation;
+    /// <summary>
+    /// La vitesse de la rotation
+    /// </summary>
     [SerializeField] private float vitesseRotation;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // La vitesse est utilisée pour déterminer un angle de rotation
         float angle = vitesseRotation * Time.deltaTime;
+        // La rotation se fait en fonction de l'angle et de l'axe
         transform.Rotate(axeRotation, angle);
     }
 }
