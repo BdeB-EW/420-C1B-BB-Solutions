944c6ff9bad32961199b3ee4d09e253e8480d929
Eric Wenaas <eric.wenaas@bdeb.qc.ca>
2023-01-30 10:29:35 -0500

    Mise-à-jour du premier exerice
    Fonctionne avec les valeurs du vecteur localScale au lieu de la magnitude.

--- Module3-Ancien/M3_Exercice1/Assets/Scripts/Gonflement.cs
+++ Module3-Ancien/M3_Exercice1/Assets/Scripts/Gonflement.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 /// <summary>
 /// Classe qui fait grandir et rapetisser la sphère
 /// 
 /// Auteurs: Enseignants de 420-1CB-BB
 /// </summary>
-public class CoeurQuiBat : MonoBehaviour
+public class Gonflement : MonoBehaviour
 {
     /// <summary>
     /// La vitesse de croissance et décroissance
@@ -19,42 +20,44 @@ public class CoeurQuiBat : MonoBehaviour
     /// Un texte pour dire si on est en train d'agrandir ou de diminuer la taille
     /// </summary>
     [SerializeField]
-    private Text textMessage;
+    private TMP_Text textMessage;
     /// <summary>
     /// Permet de déterminer si on agrandit la sphère ou on la réduit
     /// </summary>
-    private bool agrandissementActif;
+    private bool gonflementActif;
 
     void Start()
     {
-        agrandissementActif = true;
-        textMessage.text = "Agrandissement";
+        gonflementActif = true;
+        textMessage.text = "On gonfle";
         transform.localScale = Vector3.one * 2;
     }
 
     void Update()
     {
         // On agrandit ou on rapetisse selon la valeur du booléen
-        if (agrandissementActif)
+        if (gonflementActif)
         {
-            transform.localScale += new Vector3(1, 1, 1) * Time.deltaTime * vitesse;
+            transform.localScale += Vector3.one * Time.deltaTime * vitesse;
         }
         else
         {
-            transform.localScale -= new Vector3(1, 1, 1) * Time.deltaTime * vitesse;
+            transform.localScale -= Vector3.one * Time.deltaTime * vitesse;
         }
 
         // On met à jour le booléen pour la prochaine itération
-        if (transform.localScale.magnitude >= 8.0f)
+        if (transform.localScale.x >= 7.0f && transform.localScale.y >= 7.0f &&
+            transform.localScale.z >= 7.0f)
         {
-            agrandissementActif = false;
-            textMessage.text = "Rétrécissement";
+            gonflementActif = false;
+            textMessage.text = "On dégonfle";
         }
 
-        if (transform.localScale.magnitude <= 2.0f)
+        if (transform.localScale.x <= 2.0f && transform.localScale.y <= 2.0f &&
+            transform.localScale.z <= 2.0f)
         {
-            agrandissementActif = true;
-            textMessage.text = "Agrandissement";
+            gonflementActif = true;
+            textMessage.text = "On gonfle";
         }
     }
 }
