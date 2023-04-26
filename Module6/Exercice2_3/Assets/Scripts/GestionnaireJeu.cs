196a3c60cd9a2de8c101cfbed974c382bd93f6ec
Eric Wenaas <eric.wenaas@bdeb.qc.ca>
2023-04-26 15:33:45 -0400

    H2023: Exercice 6 3 complété.

--- Module6/Exercice2_3/Assets/Scripts/GestionnaireJeu.cs
+++ Module6/Exercice2_3/Assets/Scripts/GestionnaireJeu.cs
@@ -9,17 +9,22 @@ public class GestionnaireJeu : MonoBehaviour
     
     [SerializeField] private TMP_Text txtManges;
     [SerializeField] private TMP_Text txtRestants;
+    [SerializeField] private float distanceMinimum;
 
     private AudioSource sonSandwich;
+    private GameObject gourmand;
 
     
     private int nbSandwichsManges = 0;
     private int nbSandwichsRestants = 0;
 
+    private int nombreEssaisMax = 20; // Le nombre d'essai maximal pour essayer de placer un sandwich
+
     void Start()
     {
 
         sonSandwich = GameObject.Find("Crounch").GetComponent<AudioSource>();
+        gourmand = GameObject.Find("Gourmand");
 
         for (int i=0; i<5; i++)
         {
@@ -60,13 +65,55 @@ public class GestionnaireJeu : MonoBehaviour
 
     public void CreerSandwich()
     {
+        for (int i=0; i<nombreEssaisMax; i++)
+        {
+            Vector3 position = GenererPositionAleatoire();
+
+            if (EstPositionValide(position))
+            {
+                Instantiate(prefabSandwich, position, Quaternion.identity);
+                nbSandwichsRestants++;
+                break;
+            }
+        }
+        Debug.Log("Aucun endroit valide pour un nouveau sandwich");
+    }
+
+    public Vector3 GenererPositionAleatoire()
+    {
         float x = Random.Range(-12f, 12f);
         float y = 2.0f;
         float z = Random.Range(-12f, 12f);
-
         Vector3 position = new Vector3(x, y, z);
+        return position;
+    }
 
-        GameObject.Instantiate(prefabSandwich, position, Quaternion.identity);
-        nbSandwichsRestants++;
+    public bool EstPositionValide(Vector3 position)
+    {
+        bool valide = true;
+        
+        float distanceJoueur = Vector3.Distance(position, gourmand.transform.position);
+
+        if (distanceJoueur < distanceMinimum)
+        {
+            valide = false;
+        }
+        else
+        {
+            GameObject[] sandwichs = GameObject.FindGameObjectsWithTag("Sandwich");
+            for (int i=0; i<sandwichs.Length; i++)
+            {
+                float distanceSandwich = Vector3.Distance(position, sandwichs[i].transform.position);
+                if (distanceSandwich < distanceMinimum)
+
+                {
+                    valide = false;
+                    break;
+                }
+            }
+
+        }
+
+        return valide;
     }
 }
