d5824867436198f1b23e896ccea29e19c44d4dae
Eric Wenaas <eric.wenaas@bdeb.qc.ca>
2023-04-26 14:22:54 -0400

    H2023: Exercice 6.1 est complété

---
+++ Module6/Exercice1/Assets/Scripts/Poulet.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Des méthodes ont été introduites
+ */
+public class Poulet : MonoBehaviour
+{
+    [SerializeField] private float vitesseMarcher = 1.2f;
+    [SerializeField] private float vitesseTourner = 150f;
+
+    private Animator animateur;
+    private AudioSource sonCoq;
+
+    void Start()
+    {
+        animateur = GetComponent<Animator>();
+        sonCoq = GameObject.Find("Cocorico").GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");  // tourner à gauche ou à droite
+        float vertical = Input.GetAxis("Vertical");      // avancer ou reculer
+
+        BougerPoulet(horizontal, vertical);
+        DeterminerAnimation(horizontal, vertical);
+        
+        // Cocorico
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            sonCoq.Play();
+        }
+    }
+
+    private void BougerPoulet(float horizontal, float vertical)
+    {
+        // Déplace le poulet vers l'avant ou vers l'arrière
+        transform.Translate(Vector3.forward * vertical * vitesseMarcher * Time.deltaTime);
+
+        // Fait tourner le poulet sur la gauche ou sur la droite
+        transform.Rotate(Vector3.up, horizontal * vitesseTourner * Time.deltaTime);
+    }
+
+    private void DeterminerAnimation(float horizontal, float vertical)
+    {
+        // Fait marcher le poulet s'il se déplace
+        bool entreeAxeHorizontal = !Mathf.Approximately(horizontal, 0f);
+        bool entreeAxeVertical = !Mathf.Approximately(vertical, 0f);
+
+        bool enMouvement = entreeAxeHorizontal || entreeAxeVertical;
+        if (enMouvement)
+        {
+            animateur.SetBool("Walk", true);
+        }
+        else
+        {
+            animateur.SetBool("Walk", false);
+        }
+
+        // Fait picorer le poulet si la barre d'espacement est appuyée
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animateur.SetBool("Eat", true);
+        }
+        else
+        {
+            animateur.SetBool("Eat", false);
+        }
+    }
+
+}
