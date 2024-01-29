8d16887d031b093d310f2a79202460ae944fdda0
Eric Wenaas <eric.wenaas@bdeb.qc.ca>
2024-01-29 14:56:14 -0500

    H2024: L'exercice 4 n'avait pas été modifié. Maintenant c'est fait.

--- Module1/M1_Exercice4/Assets/Scripts/SalutationPersonnes.cs
+++ Module1/M1_Exercice4/Assets/Scripts/SalutationPersonnes.cs
@@ -1,27 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-/**
- * Programme qui salue deux personnes.
- * 
- * Auteurs: Enseignants de 420-C1B-BB
- */
 
 public class SalutationPersonnes : MonoBehaviour
 {
-    [SerializeField] private string nomPersonne1;   // Le nom de la première personne
-    [SerializeField] private string nomPersonne2;   // Le nom de la deuxième personne
-
+    public TMP_Text messageSalutation;
+    public string nomPersonne;
+    
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Bonjour à " + nomPersonne1 + " et à " + nomPersonne2);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        messageSalutation.text = "Bonjour " + nomPersonne;
     }
 }
